540a4962a353ac43d38039b6394b2aeb304cd44c
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-25 20:51:02 +0530

    Create sorted.java

---
+++ sorted.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Simple{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int ar[]=new int[n];
+for(i=0;i<n,i++){
+ar[i]=sc.nextInt();
+}
