393e89c9c38c5e722fb1b159820bbd4c5031126f
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-26 03:28:56 -0800

    Update sorted.java

--- sorted.java
+++ sorted.java
@@ -4,6 +4,28 @@ public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 int ar[]=new int[n];
-for(i=0;i<n,i++){
+for(i=0;i<n;i++){
 ar[i]=sc.nextInt();
 }
+for(i=0;i<n;i++)
+{
+  for(j=0;j<n;j++){
+    if(a[i+1]<a[i])
+    {
+      temp=a[i];
+      a[i]=a[i+1];
+      a[i+i]=temp;}
+  }
+  else{
+    break;
+  }
+}
+}
+  for(i=0;i<n;i++)
+  {
+    System.out.println(ar[i]);
+  }
+}
+}
+  
+      
