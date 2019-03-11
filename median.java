a35d879f60ea6b347e59eed8ff847e22b801d670
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-03-11 22:25:51 +0530

    Update median.java

--- median.java
+++ median.java
@@ -1,43 +1,27 @@
 import java.util.*;
-class Simple{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-int i,j,t;
+int i,t;
+float j;
 int a[]=new int[n];
+int l=a.length;
 for(i=0;i<n;i++)
 {
 a[i]=sc.nextInt();
 }
-for(i=0;i<n-1;i++)
-{
-int flag=0;
-for(j=0;j<n-1-i;j++)
-{
-if(a[j]>a[j+1])
-{
-t=a[j];
-a[j]=a[j+1];
-a[j+1]=t;
-flag=1;
+Arrays.sort(a);
+if(a.length%2!=0){
+i=(l/2);
+System.out.println(a[i]);
 }
-}
-if(flag==0)
-{
-break;
-}
-}
-if(a.length()%2!=0){
-int l=a.lenght();
-int med=(l/2)+1;
-System.out.println(a[med]);
-}
-else
-{
-int l=a.lenght();
-int med=(a[l]+a[l+1])/2;
-System.out.println(med);
+else {
+ j=a.length/2;
+ float s=a[j]+a[j-1];
+ float t=s/2;
+System.out.println(t);
 }
 }
 }
-
