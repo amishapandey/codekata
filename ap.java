537edae726738dce935698b888d8b120ac893718
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-03-13 14:59:16 +0530

    Update ap.java

--- ap.java
+++ ap.java
@@ -2,7 +2,15 @@ import java.util.*;
 class Simple{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
-int a=sc.nextInt();
-int b=sc.nextInt();
-int c=sc.nextInt();
-
+System.out.println("Enter the first,difference,terms of series");
+int f=sc.nextInt();
+int diff=sc.nextInt();
+int t=sc.nextInt();
+int v,sum=0;
+v=f;
+for(i=0;i<t;i++)
+sum+=v;
+v=v+diff;
+}
+System.out.println(sum);
+}
