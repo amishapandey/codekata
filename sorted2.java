588fafb81a8764e1e7160f032e50668cf6f3aed4
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-01 20:15:29 +0530

    Create sorted2.java

---
+++ sorted2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Simple{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+  int i,j,temp;
+for(i=0;i<n;i++){
+a[i]=sc.nextInt();
+}
+for(i=0;i<n-1;i++)
+{
+  int flag=0;
+  for(j=0;j<n-1-i;j++){
+    if(a[j+1]<a[j])
+    {
+      temp=a[j];
+      a[j]=a[j+1];
+      a[j+1]=temp;
+      flag=1;
+   }
+  }
+
+  if(flag==0)
+  {
+    break;
+  }
+}
+  for(i=0;i<n;i++)
+  {
+    System.out.print(a[i]+" ");
+  }
+}
+}
+  
+      
