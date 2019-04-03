b20ea6dc83ca52e380ac2ebc1a2a70b79e9a4b5c
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-03 23:25:19 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int i,f=1;
+	    for(i=1;i<=n;i++){
+	        f=f*i;
+	    }
+		System.out.println(f);
+	}
+}
