279b737418c0c0314765a54428a089f638b7e2ba
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-03 23:54:33 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int num=sc.nextInt();
+	    int rem,rev=0;
+	    while(num!=0){
+	        rem=num%10;
+	        rev=rev*10;
+	        rev=rev+rem;
+	        num=num/10;
+	    }
+		System.out.println(rev);
+	}
+}
