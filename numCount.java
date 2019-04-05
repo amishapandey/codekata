c176d82ccd305471a6a11bd10dd8a6025305fcf9
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-05 09:51:29 +0530

    Create numCount.java

---
+++ numCount.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    for(i=0;i<s.length();i++){
+	        if(char)
+	    }
+		System.out.println("Hello World");
+	}
+}
