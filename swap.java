7b5c1c8cd0a9eafb05ab5fe0e660c05bed7530ed
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-05 16:54:17 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -5,9 +5,10 @@ int main()
     int a,b,temp;
     printf("enter two numbers");
     scanf("%d%d",&a,&b);
-    temp=a;
-    a=b;
-    b=temp;
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    
     printf("%d\n%d",a,b);
 
     return 0;
