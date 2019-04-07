f02357be04227fd172316af56eddcc62eb81404d
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-07 07:47:07 +0530

    Create fiboNew.java

---
+++ fiboNew.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int i,fib;
+	    for(i=1;i<=5;i++)
+		System.out.println(fib);
+	}
+}
