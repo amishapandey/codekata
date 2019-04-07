b29ae7860614189642cfce8cf6c533a79f0b0dad
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-07 08:29:50 +0530

    Update numCount.java

--- numCount.java
+++ numCount.java
@@ -4,9 +4,12 @@ public class Main
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    String s=sc.nextLine();
+	    int i,count=0;
 	    for(i=0;i<s.length();i++){
-	        if(char)
+	        if(s.charAt(i)==' ')
+	        count=count+1;
 	    }
-		System.out.println("Hello World");
+	    count=count+1;
+		System.out.println(count);
 	}
 }
