87788c4966006838365f37661dde276b81acc455
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-07 23:02:54 +0530

    Create stringNo.java

---
+++ stringNo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String str=sc.nextLine();
+	    
+	    int n=sc.nextInt();
+	    int i,count=0;
+	    for(i=0;i<n;i++){
+	   System.out.println(str);
+	      }
+}
+}
