1a143b4266f4dbb57812a696a914b2f33af14abc
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-09 11:17:27 +0530

    Create numRange.java

---
+++ numRange.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+        if((n>0 && n<9) || (n==10))
+	  System.out.println("yes");
+	  else
+	  System.out.println("no");
+	  
+	    
+	
+	}
+}
