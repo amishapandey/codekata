814c6d1b8c99ae51889e2caf17d7d4a98fd1c14e
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-09 10:26:10 +0530

    Create avger.java

---
+++ avger.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int[] arr=new int[n];
+	    int i;
+	    for(i=0;i<n;i++)
+	    {
+	        arr[i]=sc.nextInt();
+	    }
+	    int sum=0;
+	    for(i=0;i<n;i++){
+	        sum=sum+arr[i];
+	    }
+		System.out.println(sum/n);
+	    
+	
+	}
+}
