40a1761aca21780467cec4f3ba854afd25cd52cc
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-13 22:02:50 +0530

    Create iso.java

---
+++ iso.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s1=sc.nextLine();
+	    String s2=sc.nextLine();
+	    char [] ch1=s1.toCharArray();
+	    char [] ch2=s2.toCharArray();
+	    int i;
+	    
+	        if((ch1[0]==ch1[1]) ||(ch1[0]==ch1[1] && ch1[1]==ch1[2]))
+	        {
+	            
+	            if((ch2[0]==ch2[1]) ||(ch2[0]==ch2[1] && ch2[1]==ch2[2])) 
+	            System.out.println("yes");
+	            else
+	            System.out.println("no");
+	            
+	            }
+	            
+	        }
+	    }
+	
+
+	               
+	           
