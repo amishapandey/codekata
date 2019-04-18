693e75de9f79b8d9541bcfb802436a38d7372dd4
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-18 12:51:00 +0530

    Create week.java

---
+++ week.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner  (System.in);
+		String s2=sc.nextLine();
+		if (s2=="Saturday" || s2=="Sunday" || s2=="saturday" || s2=="sunday")
+		{
+		System.out.println("yes");
+		}
+		else
+		{
+		System.out.println("no");
+		}
+		
+	}
+}
