ba0999a33f7da327f2c78a8822dd7e3b5a4079a2
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-19 09:55:07 +0530

    Create pro.java

---
+++ pro.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input=new Scanner(System.in);
+         int n=input.nextInt();
+         int m=input.nextInt();
+         int p=m*n;
+         if(p%2==0)
+         System.out.print("even");
+         else
+         System.out.print("odd");
+         
+		
+	}
+}
