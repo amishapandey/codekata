c1ce4b1f7cc0ca1a7d5c4bc4c7e7b6272550cbc3
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-19 09:51:56 +0530

    Create nearEve.java

---
+++ nearEve.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input=new Scanner(System.in);
+         int n=input.nextInt();
+         if(n%2==0)
+         System.out.print(n);
+         else
+         System.out.print(n-1);
+         
+		
+	}
+}
