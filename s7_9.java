40df8591a52b9716b7331bab71d1846c64528a6d
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-21 00:21:36 +0530

    Create s7_9.java

---
+++ s7_9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int m=sc.nextInt();
+	    int d=n-m;
+	    if(d%2==0)
+	    System.out.println("even");
+	    else
+	    System.out.println("odd");
+	    
+	}
+}
