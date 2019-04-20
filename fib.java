3c06fb80504b12a592159eaffb4f1d02d0ee785e
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-20 19:36:59 +0530

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int a=0,b=0,c=1,i;
+	    for(i=1;i<=n;i++){
+	      a=b;
+	      b=c;
+	      c=a+b;
+	      System.out.print(a);
+	      System.out.print(' ');
+	  }
+}
+}
