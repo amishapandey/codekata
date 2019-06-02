c30ff7abe005bbb3c3f38ff08657c7c5f66f676e
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-06-02 14:08:38 +0530

    Create S9_q4.java

---
+++ S9_q4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    char ch[]=s.toCharArray();
+	      System.out.println(ch[0]+0);
+	  }
+	  
+}
