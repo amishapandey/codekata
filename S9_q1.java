95c1e2b3317caef69a8da6390523197524dc12c4
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-06-02 19:15:36 +0530

    Create S9_q1.java

---
+++ S9_q1.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	     int i;
+	      int arr[]=new int[2];
+	        for(i=0;i<2;i++)
+	        arr[i]=sc.nextInt();
+	   
+	        System.out.println(Math.abs(arr[0]-arr[1]));
+	    
+}
+}
