64be80f77532a90dcb2ad1c2c11bd56b71cd7fda
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-06-02 19:32:02 +0530

    Create S9_q2.java

---
+++ S9_q2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	     int i;
+	     float a=sc.nextFloat();
+	     float b=sc.nextFloat();
+	    System.out.printf("%.5f",a*b);
+	    
+}
+}
